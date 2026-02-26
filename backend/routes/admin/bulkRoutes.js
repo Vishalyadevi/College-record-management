461a39b34641ae8625ba347730e7e08b2f82d412
Vishalyadevi <mvishalyadevi@gmail.com>
2026-02-26 10:10:48 +0530

    Delete backend directory

--- backend/routes/admin/bulkRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { upload, bulkUpload } from "../../controllers/admin/bulkController.js";
-
-const router = express.Router();
-
-router.post("/upload", upload.single("file"), bulkUpload);
-
-export default router;
